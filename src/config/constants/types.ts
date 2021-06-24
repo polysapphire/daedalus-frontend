2a2faba5573c126de0bae985cd0dfa5106c9ab3a
DaedalusFinance <86154134+polydaedalus@users.noreply.github.com>
2021-06-24 23:47:38 +0300

    Update types.ts

--- src/config/constants/types.ts
+++ src/config/constants/types.ts
@@ -26,7 +26,7 @@ export enum QuoteToken {
   'BUSD' = 'BUSD',
   'TWT' = 'TWT',
   'UST' = 'UST',
-  'STONK' = 'STONK',
+  'NYX' = 'NYX',
   'WBTC' = 'WBTC',
   'WMATIC' = 'WMATIC',
   'QUICK' = 'QUICK',
